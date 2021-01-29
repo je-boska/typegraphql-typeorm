fea40ea91e358c6baf87032c6ce90f55285e7a3e
joneirikboska <joneirikboska@gmail.com>
2021-01-29 11:37:29 +0100

    Create Book model and object type.

---
+++ src/models/Book.ts
@@ -0,0 +1,23 @@
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { ObjectType, Field, ID } from 'type-graphql'
+
+// This class represent both the GraphQL object type, and the db model
+@Entity()
+@ObjectType()
+export class Book extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: string
+
+  @Field(() => String)
+  @Column()
+  title: string
+
+  @Field(() => String)
+  @Column()
+  author: string
+
+  @Field(() => Boolean)
+  @Column({ default: false })
+  isPublished: boolean
+}
