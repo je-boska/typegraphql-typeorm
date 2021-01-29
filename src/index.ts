f9ab7c0debb8b107f53492c4697351ad65ffb666
joneirikboska <joneirikboska@gmail.com>
2021-01-29 11:26:07 +0100

    Set up app, create basic hello world resolver.

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata'
+import 'dotenv-safe/config'
+import { createConnection } from 'typeorm'
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+import { BookResolver } from './resolvers/BookResolver'
+
+async function main() {
+  const connection = await createConnection({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: ['.src/models/*.ts'],
+    synchronize: true,
+  })
+  const schema = await buildSchema({
+    resolvers: [BookResolver],
+  })
+  const server = new ApolloServer({ schema })
+  await server.listen(4000)
+  console.log('Server is running on port 4000')
+}
+main()
