e9cf50d8e518b653c3cac6bc0e073d80e7bc09a1
joneirikboska <joneirikboska@gmail.com>
2021-02-01 11:38:59 +0100

    Create delete book modal.

--- web/src/components/Book.tsx
+++ web/src/components/Book.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
-import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react'
 import { DeleteIcon } from '@chakra-ui/icons'
 import { BookType } from '../types'
 
@@ -10,26 +24,46 @@ interface BookProps {
 }
 
 export const Book: React.FC<BookProps> = ({ book, deleteBook, selectBook }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Flex key={book.id}>
-      <Button
-        m='0px 20px'
-        onClick={() => deleteBook(book.id)}
-        backgroundColor='transparent'
-      >
-        <DeleteIcon />
-      </Button>
-      <Box width='500px' paddingBottom='30px'>
-        <Heading size='lg' onClick={() => selectBook(book)} cursor='pointer'>
-          {book.title}
-        </Heading>
-        <Text>{book.author}</Text>
-        {book.isPublished ? (
-          <Text color='blue.500'>Published</Text>
-        ) : (
-          <Text color='red.300'>Not Published</Text>
-        )}
-      </Box>
-    </Flex>
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete Book</ModalHeader>
+          <ModalBody>Are you sure you want to delete this book?</ModalBody>
+          <ModalCloseButton />
+          <ModalFooter>
+            <Button
+              onClick={() => {
+                onClose()
+                deleteBook(book.id)
+              }}
+              mr={3}
+            >
+              Yes
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Flex key={book.id}>
+        <Button m='0px 20px' onClick={onOpen} backgroundColor='transparent'>
+          <DeleteIcon />
+        </Button>
+        <Box width='500px' paddingBottom='30px'>
+          <Heading size='lg' onClick={() => selectBook(book)} cursor='pointer'>
+            {book.title}
+          </Heading>
+          <Text>{book.author}</Text>
+          {book.isPublished ? (
+            <Text color='blue.500'>Published</Text>
+          ) : (
+            <Text color='red.300'>Not Published</Text>
+          )}
+        </Box>
+      </Flex>
+    </>
   )
 }
